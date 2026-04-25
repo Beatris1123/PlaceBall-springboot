4f844360da4877a0fbcf0dbecf1d0bdea760ca19
Beatris1123 <kimkbh234@naver.com>
2026-04-25 21:21:34 +0900

    2026/04/25

--- frontend/shared/const.ts
+++
@@ -1,8 +0,0 @@
-/**
- * shared/const.ts - 공유 상수
- *
- * 원본: baseballai-bicevcq9/shared/const.ts (동일)
- * 백엔드/프론트엔드 공통 상수
- */
-export const COOKIE_NAME = "app_session_id";
-export const ONE_YEAR_MS = 1000 * 60 * 60 * 24 * 365;
